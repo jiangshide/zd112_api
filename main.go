5f52a1b7464f4283053e21ff34a3de2806a3820a
jankey <18311271399@163.com>
2017-12-22 18:45:15 +0800

    init

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "zd112_api/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
